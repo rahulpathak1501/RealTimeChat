746d336db560b0830572e33297495081efb4637e
rahulpathak1501 <rahul150101130015@gmail.com>
2025-06-25 10:47:40 +0530

    fixing cors error

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://realtimechat-frontend-2pb5.onrender.com/",
+  "https://realtimechat-frontend-2pb5.onrender.com",
 ];
 
 const io = new Server(server, {
